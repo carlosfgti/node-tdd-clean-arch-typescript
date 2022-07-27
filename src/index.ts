11a541d9849b9c80080c78830d49be67bfbf12bc
Carlos Ferreira <carlos@especializati.com.br>
2022-07-27 10:34:16 -0300

    First Commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+class User {
+  sayHello (name?: string): string {
+    return `Hello ${name?.toUpperCase} ?? Anonymous`;
+  }
+}
+
+const user = new User();
+user.sayHello('Carlos')
+user.sayHello()
