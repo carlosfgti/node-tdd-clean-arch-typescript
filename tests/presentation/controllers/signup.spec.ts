a72c57e599942954c719905a0c9a75a17c344f7d
Carlos Ferreira <carlos@especializati.com.br>
2022-08-01 10:59:51 -0300

    test: ensure SingUpController returns 400 if no name is provided

---
+++ tests/presentation/controllers/signup.spec.ts
@@ -0,0 +1,17 @@
+import { SingUpController } from '@/controllers/signup'
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SingUpController()
+    const httpRequest = {
+      body: {
+        // name: 'example name',
+        email: 'carlos@example.com',
+        password: '65432312312',
+        passwordConfirmation: '65432312312'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
