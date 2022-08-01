eb96499e467f7c6ab8562680034a679d8129d0d6
Carlos Ferreira <carlos@especializati.com.br>
2022-08-01 11:06:59 -0300

    feat: ensure SingUpController return an error if no name is provided

--- src/controllers/signup.ts
+++ src/controllers/signup.ts
@@ -1,7 +1,8 @@
 export class SingUpController {
   handle (httpRequest: any): any {
     return {
-      statusCode: 400
+      statusCode: 400,
+      body: new Error('name is required')
     }
   }
 }
