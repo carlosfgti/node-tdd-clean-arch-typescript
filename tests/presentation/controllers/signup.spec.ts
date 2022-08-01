093295e81716f84c2a7f043c7e528992dd504330
Carlos Ferreira <carlos@especializati.com.br>
2022-08-01 11:10:41 -0300

    test: ensure SingUpController return an error if no email is provided

--- tests/presentation/controllers/signup.spec.ts
+++ tests/presentation/controllers/signup.spec.ts
@@ -15,4 +15,19 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('name is required'))
   })
+
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SingUpController()
+    const httpRequest = {
+      body: {
+        name: 'example name',
+        // email: 'carlos@example.com',
+        password: '65432312312',
+        passwordConfirmation: '65432312312'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('email is required'))
+  })
 })
